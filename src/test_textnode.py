dccb62711599cb98347253e3c1798d7e275f2026
KiefBC <kiefhay642@outlook.com>
2025-05-03 21:32:03 -0700

    Added some basic unittests

---
+++ src/test_textnode.py
@@ -0,0 +1,38 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq2(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://www.google.com")
+        self.assertEqual(node, node2)
+
+    def test_ne(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_ne2(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_ne3(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://www.yahoo.com")
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(repr(node), "TextNode(This is a text node, TextType.BOLD, None)")
+
+
+if __name__ == "__main__":
+    unittest.main()
