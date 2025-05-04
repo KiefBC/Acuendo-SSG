039d029960add6a04ced677ba4e3bc6af264265f
KiefBC <kiefhay642@outlook.com>
2025-05-03 21:21:52 -0700

    Initial Commits for main and textnode

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+def main():
+    text_node = TextNode("Hello", "Bold", "https://www.google.com")
+    print(text_node)
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "Normal",
+    BOLD = "Bold",
+    ITALIC = "Italic",
+    CODE = "Code"
+    URL = "URL"
+    IMG = "Image"
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
