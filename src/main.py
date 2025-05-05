ccd5a519b721a3cc416ef006eaece2c6b172ee47
KiefBC <kiefhay642@outlook.com>
2025-05-05 13:56:29 -0700

    recursive copy working
    Signed-off-by: KiefBC <kiefhay642@outlook.com>

--- src/main.py
+++ src/main.py
@@ -1,15 +1,50 @@
 import os
-
-from textnode import TextNode
+import shutil
 
 
 def main():
-    text_node = TextNode("Hello", "Bold", "https://www.google.com")
-    print(text_node)
+    root_dir = os.path.abspath(os.path.join(os.getcwd()))
+    static_dir = os.path.join(root_dir, "static")
+    public_dir = os.path.join(root_dir, "public_test")
+    copy_files(static_dir, public_dir)
 
 
-def static_to_public():
-    pass
+def copy_files(src: str, dest: str):
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+
+        if os.path.isdir(src_path):
+            copy_files(src_path, dest_path)
+        else:
+            shutil.copy2(src_path, dest_path)
+
+
+def delete_dir_files(dir):
+    if len(dir) == 0:
+        return True
+
+    os.remove(dir[0])
+    remaining_files = dir[1:]
+
+
+def static_to_public(path):
+    contents = None
+    file_paths = []
+
+    if os.path.exists(path):
+        contents = os.listdir(path)
+        for _, data in enumerate(contents):
+            file_path = f"{path}/{data}"
+            file_paths.append(file_path)
+    else:
+        os.mkdir(path)
+
+    print("File Paths:")
+    print(file_paths)
 
 
 if __name__ == "__main__":
