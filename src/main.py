5d1604b462b0b7df219639860be4d9199db2bad4
KiefBC <kiefhay642@outlook.com>
2025-05-05 14:30:57 -0700

    recursive copy and delete works
    Signed-off-by: KiefBC <kiefhay642@outlook.com>

--- src/main.py
+++ src/main.py
@@ -4,47 +4,60 @@ import shutil
 
 def main():
     root_dir = os.path.abspath(os.path.join(os.getcwd()))
-    static_dir = os.path.join(root_dir, "static")
-    public_dir = os.path.join(root_dir, "public_test")
-    copy_files(static_dir, public_dir)
+    src_path = os.path.join(root_dir, "static")
+    dest_path = os.path.join(root_dir, "public")
+
+    print("We are building the static files")
+    print("================================\n")
+    static_to_public(src_path, dest_path)
 
 
 def copy_files(src: str, dest: str):
-    if not os.path.exists(dest):
-        os.makedirs(dest)
-
+    print(f"Copying files from {src} to {dest}")
     for item in os.listdir(src):
         src_path = os.path.join(src, item)
         dest_path = os.path.join(dest, item)
 
         if os.path.isdir(src_path):
+            if not os.path.exists(dest_path):
+                os.makedirs(dest_path)
             copy_files(src_path, dest_path)
         else:
+            print(f"We are copying: {src_path}")
             shutil.copy2(src_path, dest_path)
 
-
-def delete_dir_files(dir):
-    if len(dir) == 0:
-        return True
-
-    os.remove(dir[0])
-    remaining_files = dir[1:]
+    return True
 
 
-def static_to_public(path):
-    contents = None
-    file_paths = []
+def delete_dir_files(src):
+    if len(os.listdir(src)) == 0:
+        return
 
-    if os.path.exists(path):
-        contents = os.listdir(path)
-        for _, data in enumerate(contents):
-            file_path = f"{path}/{data}"
-            file_paths.append(file_path)
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+
+        if os.path.isdir(src_path):
+            delete_dir_files(src_path)
+        else:
+            print(f"We are deleting: {item}")
+            os.remove(src_path)
+
+
+def static_to_public(src, dest):
+    if not os.path.exists(dest):
+        print(f"Creating directory: {dest}")
+        os.makedirs(dest)
     else:
-        os.mkdir(path)
+        print(f"Deleting files in: {dest}")
+        print(f"{'#' * len(dest)}###################")
+        delete_dir_files(dest)
 
-    print("File Paths:")
-    print(file_paths)
+    print(f"\nCopying files from {src} to {dest}")
+    print(f"{'#' * (len(src) + len(dest))}#######################")
+    if copy_files(src, dest):
+        print(f"\nFiles copied from {src} to {dest}")
+    else:
+        print(f"\nFailed to copy files from {src} to {dest}")
 
 
 if __name__ == "__main__":
